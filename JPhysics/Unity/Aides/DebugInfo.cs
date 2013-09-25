2d1d0acf4acb71b640b529bf9611004f00d463de
Oleg <pod4444@yandex.ru>
2013-09-25 20:41:42 +0400

    DebugInfo Version

--- JPhysics/Unity/Aides/DebugInfo.cs
+++ JPhysics/Unity/Aides/DebugInfo.cs
@@ -6,15 +6,18 @@
     class DebugInfo : MonoBehaviour
     {
         double lastTime;
+        string version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
         void FixedUpdate()
         {
             lastTime = JPhysics.World.DebugTimes.Sum();
+
         }
 
         void OnGUI()
         {
             GUILayout.Label("Summary physics time: " + lastTime + " ms");
+            GUILayout.Label("Version JPhysics: " + version);
         }
     }
 }
